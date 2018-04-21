1fc69de1264a982b675318cc6348b2062a425c5b
Gretel Martinez <gretelmn@gmail.com>
2018-04-21 11:22:09 -0400

    customer.js

---
+++ customer.js
@@ -0,0 +1,9 @@
+function Customer(name,phone,email,destination,id){
+    this.name = name;
+    this.phone = phone;
+    this.email = email;
+    this.destination = destination;
+    this.id = id;
+}
+
+module.exports = Customer;
